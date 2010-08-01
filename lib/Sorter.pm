22eb4273e24a3cdc63a52e18574e3c6c272e9d95
Taro Sekiyama <t-sekiym@kuis.kyoto-u.ac.jp>
2010-08-01 18:59:27 +0900

    implements Sorter.pm

--- lib/Sorter.pm
+++ lib/Sorter.pm
@@ -1,1 +1,52 @@
 # implement this
+package Sorter;
+
+use strict;
+use warnings;
+
+sub new {
+  bless [], 'Sorter';
+}
+
+sub set_values {
+  my $self = shift;
+  @$self = @_;
+}
+
+sub get_values {
+  @{$_[0]};
+}
+
+sub sort {
+  sub swap {
+    my $array = shift;
+    my $l = shift;
+    my $r = shift;
+    my $tmp = $array->[$l];
+    $array->[$l] = $array->[$r];
+    $array->[$r] = $tmp;
+  }
+
+  my $self = shift;
+  my ($l, $r) = @_;
+  $l = defined($l) ? $l : 0;
+  $r = defined($r) ? $r : $#$self;
+
+  return if ($r <= $l);
+
+  my $pv = $self->[$l];
+  my $left =  $l + 1;
+  my $right = $r;
+  while (1) {
+    ++$left  while ($left  <= $r && $self->[$left]  <= $pv);
+    --$right while ($right >= $l && $self->[$right] >= $pv);
+    last     if ($left > $right);
+    &swap($self, $left++, $right--);
+  }
+  &swap($self, $l, $right--) if ($right > $l);
+  $self->sort($left, $r);
+  $self->sort($l, $right);
+}
+
+1;
+__END__
